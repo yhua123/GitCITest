cf9be0ae78ad9dba6b104eb80acb319f13e431bd
yhua <a@b.com>
2016-05-13 10:46:33 +0800

    test.go exit(1)

--- tests/test.go
+++ tests/test.go
@@ -23,7 +23,7 @@ func main() {
             os.Exit(1)
         }
 //        fmt.Printf("%s", b)
-        if strings.Contains(string(b), "HA") {
+        if strings.Contains(string(b), "HAlala") {
           fmt.Printf("sucess!\n")
         } else {
             os.Exit(1)
